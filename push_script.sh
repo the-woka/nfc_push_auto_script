21a564aa3bda965b4cf694597b9d10f4249576a9
the-woka <ois.woka23@gmail.com>
2026-04-25 01:07:04 +0700

    Create auto push script

---
+++ push_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Start Pushing the Code!"
+
+read "Input commit message: " commit_msg
+if [-z "$commit_msg"]; then
+    echo "Commit can't be empty"
+    exit 1
+fi
+
+read "Input name branch (Enter for auto main): " branch_name
+branch_name=${branch_name: main}
+
+git add .
+git commit -m commit_msg
+git push -u origin branch_name
+
+echo "Done Push!"
